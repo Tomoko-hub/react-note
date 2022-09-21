8fc3d8bb9183f46bf94b3edeb6dff38efcf9c60e
Tomoko-hub <haisaitomoco@gmail.com>
2022-09-21 08:54:22 +0300

    create note properies to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
 
 function App() {
+
+  const [ notes, setNotes ] = useState([]);
+
+  const onAddNote =()=> {
+    console.log("clicked");
+    const newNote = {
+      id: 1,
+      title: "this is the title of note. topics yeah!",
+      content: "this is the content of note. here iwe go.",
+      color: "green",
+      modDate: Date.now(),
+    };
+    setNotes([...notes, newNote]);
+    console.log(notes);
+  }
+
   return (
     <div className="App">
-      <Sidebar />
+      <Sidebar onAddNote={onAddNote} />
       <Main />
     </div>
   );
