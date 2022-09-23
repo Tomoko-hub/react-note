3f756f1b8ed21efc2d41b4f7b4cb4710ebcca46f
Tomoko-hub <haisaitomoco@gmail.com>
2022-09-23 08:28:35 +0300

    add change background color property to sidebar component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import uuid from "react-uuid";
 function App() {
 
   const [ notes, setNotes ] = useState([]);
+  const [ activeNote, setActiveNote ] = useState([false]);
 
   const onAddNote =()=> {
     console.log("clicked");
@@ -28,7 +29,13 @@ function App() {
 
   return (
     <div className="App">
-      <Sidebar onAddNote={onAddNote} notes={notes} onDeleteNote={onDeleteNote} />
+      <Sidebar 
+        onAddNote={onAddNote} 
+        notes={notes} 
+        onDeleteNote={onDeleteNote}
+        activeNote={activeNote}
+        setActiveNote={setActiveNote}
+       />
       <Main />
     </div>
   );
